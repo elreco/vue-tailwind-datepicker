cf0c694d53095334fde334617232eb2cf88f3295
Alexandre Le Corre <a.elreco@gmail.com>
2022-09-06 17:57:55 +0200

    add favicon

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ export default {
   title: 'Vue Tailwind Datepicker',
   description: 'Components, composables and configurations for Vue 3',
   themeConfig: {
+    logo: 'https://github.com/elreco/vue-tailwind-datepicker/blob/main/docs/logo.png?raw=true',
     socialLinks: [{ icon: 'github', link: 'https://github.com/elreco/vue-tailwind-datepicker' }],
     footer: {
       message: 'Released under the MIT License.',
