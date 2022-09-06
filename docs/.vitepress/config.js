781875bdc1631df330cde7ae299436f3b3a9964e
Alexandre Le Corre <a.elreco@gmail.com>
2022-09-06 19:09:59 +0200

    add base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,6 @@
 export default {
   title: 'Vue Tailwind Datepicker',
+  base: '/',
   description: 'Components, composables and configurations for Vue 3',
   themeConfig: {
     logo: 'https://github.com/elreco/vue-tailwind-datepicker/blob/main/docs/logo.png?raw=true',
@@ -13,45 +14,6 @@ export default {
         text: 'Get Started',
         items: [{ text: 'Installation', link: '/installation' }]
       },
-      {
-        text: 'Components',
-        collapsible: true,
-        items: [
-          { text: 'Album', link: '/components/album' },
-          { text: 'Breadcrumb', link: '/components/breadcrumb' },
-          { text: 'Button', link: '/components/button' },
-          { text: 'Card', link: '/components/card' },
-          { text: 'Color Indicator', link: '/components/color-indicator' },
-          { text: 'Confirm Button', link: '/components/confirm-button' },
-          { text: 'Detail Title', link: '/components/detail-title' },
-          { text: 'Dropdown', link: '/components/dropdown' },
-          { text: 'Headline', link: '/components/headline' },
-          { text: 'Loader', link: '/components/loader' },
-          { text: 'Modal', link: '/components/modal' },
-          { text: 'Notification', link: '/components/notification' },
-          { text: 'Pagination', link: '/components/pagination' },
-          { text: 'Popover', link: '/components/popover' },
-          { text: 'Progress Bar', link: '/components/progress-bar' },
-          { text: 'Radio Group', link: '/components/radio-group' },
-          { text: 'Selector', link: '/components/selector' },
-          { text: 'Tab', link: '/components/tab' },
-          { text: 'Table', link: '/components/table' },
-          { text: 'Tip', link: '/components/tip' },
-          { text: 'Tooltip', link: '/components/tooltip' }
-        ]
-      },
-      {
-        text: 'Form Components',
-        collapsible: true,
-        items: [
-          { text: 'Checkbox', link: '/components/form/checkbox' },
-          { text: 'Color Picker', link: '/components/form/color-picker' },
-          { text: 'Image Uploader', link: '/components/form/image-uploader' },
-          { text: 'Input', link: '/components/form/input' },
-          { text: 'Radio', link: '/components/form/radio' },
-          { text: 'Select', link: '/components/form/select' }
-        ]
-      }
     ],
     nav: [
       { text: 'Home', link: '/' },
