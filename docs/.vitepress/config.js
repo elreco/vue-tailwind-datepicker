92d5761845078a3b18c358cc6a197bfda6ae6618
Alexandre Le Corre <a.elreco@gmail.com>
2022-09-06 21:15:47 +0200

    Better deployment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,6 +1,5 @@
 export default {
   title: 'Vue Tailwind Datepicker',
-  base: '/vue-tailwind-datepicker/',
   description: 'Components, composables and configurations for Vue 3',
   themeConfig: {
     logo: 'https://github.com/elreco/vue-tailwind-datepicker/blob/main/docs/logo.png?raw=true',
