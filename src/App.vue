f4d1d493755c254c861b0abd19292d923e4f31e8
Julien Georget <julien.georget@hikoala.com>
2023-09-12 17:31:42 +0200

    feat(locale): add locale selector for development

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import dayjs from "dayjs";
 import { ref } from "vue";
 import VueTailwindDatePicker from "./VueTailwindDatePicker.vue";
@@ -7,28 +7,26 @@ const dateValue = ref({
   startDate: dayjs().format("YYYY-MM-DD HH:mm:ss"),
   endDate: dayjs().format("YYYY-MM-DD HH:mm:ss"),
 });
+
+const currentLocale = ref('es')
+const locales = ['en', 'es', 'de']
 </script>
 
 <template>
   <div class="p-10 bg-sky-50 min-h-screen">
+    <label>Choose one locale
+      <select v-model="currentLocale" name="language" class="mb-6">
+        <option v-for="locale in locales" :key="locale" :value="locale">{{ locale }}</option>>
+      </select>
+    </label>
     <div class="grid grid-rows-2 gap-4">
-      <VueTailwindDatePicker
-        v-model="dateValue"
-        @select:month="onSelectSomething($event)"
-        @select:year="onSelectSomething($event)"
-        @select:right:month="onSelectSomething($event)"
-        @select:right:year="onSelectSomething($event)"
-        @click:prev="onClickSomething($event)"
-        @click:next="onClickSomething($event)"
-        @click:right:prev="onClickSomething($event)"
-        @click:right:next="onClickSomething($event)"
-      />
+      <VueTailwindDatePicker v-model="dateValue" :i18n="currentLocale" @select:month="onSelectSomething($event)"
+        @select:year="onSelectSomething($event)" @select:right:month="onSelectSomething($event)"
+        @select:right:year="onSelectSomething($event)" @click:prev="onClickSomething($event)"
+        @click:next="onClickSomething($event)" @click:right:prev="onClickSomething($event)"
+        @click:right:next="onClickSomething($event)" />
 
-      <VueTailwindDatePicker
-        v-model="dateValue.startDate"
-        as-single
-        i18n="en"
-      />
+      <VueTailwindDatePicker v-model="dateValue.startDate" as-single :i18n="currentLocale" />
     </div>
   </div>
 </template>
