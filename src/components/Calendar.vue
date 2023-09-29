63ed3f0a62a964327b3c4b1608b9b2a03f8ef44f
Julien Georget <julien.georget@hikoala.com>
2023-09-29 15:39:26 +0200

    feat(calendar): add new props to show week number in calendar

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -23,6 +23,7 @@ defineProps<{
     setYear: ($event: number) => void
   }
   weeks: string[]
+  weekNumber: boolean
   asRange: boolean
 }>()
 
@@ -44,32 +45,34 @@ const atMouseOver = injectStrict(atMouseOverKey)
       leave-active-class="transition-opacity ease-in duration-200" leave-from-class="opacity-100"
       leave-to-class="opacity-0"
     >
-      <div
-        v-for="(date, keyDate) in calendar.date()" :key="keyDate" class="relative"
-        :class="{ 'vtd-tooltip': asRange && date.duration() }" :data-tooltip="`${date.duration()}`"
-      >
-        <transition
-          enter-from-class="opacity-0" enter-to-class="opacity-100"
-          enter-active-class="transition-opacity ease-out duration-200"
-          leave-active-class="transition-opacity ease-in duration-150" leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
-        >
-          <span
-            v-if="isBetweenRange(date) || date.hovered()"
-            class="absolute bg-vtd-primary-100 bg-opacity-60 dark:bg-vtd-secondary-700 dark:bg-opacity-50"
-            :class="betweenRangeClasses(date)"
+      <template v-for="(date, keyDate) in calendar.date()" :key="keyDate">
+        <div v-if="keyDate % 7 === 0 && weekNumber" class="col-span-7 border-b relative dark:border-vtd-secondary-600">
+          <span class="absolute -left-2 top-1/2 -translate-y-2/4 bg-white dark:bg-vtd-secondary-800 text-[8px] pr-2 text-vtd-secondary-400">{{ date.week() }}</span>
+        </div>
+        <div class="relative" :class="{ 'vtd-tooltip': asRange && date.duration() }" :data-tooltip="`${date.duration()}`">
+          <transition
+            enter-from-class="opacity-0" enter-to-class="opacity-100"
+            enter-active-class="transition-opacity ease-out duration-200"
+            leave-active-class="transition-opacity ease-in duration-150" leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+          >
+            <span
+              v-if="isBetweenRange(date) || date.hovered()"
+              class="absolute bg-vtd-primary-100 bg-opacity-60 dark:bg-vtd-secondary-700 dark:bg-opacity-50"
+              :class="betweenRangeClasses(date)"
+            />
+          </transition>
+          <button
+            type="button"
+            class="vtd-datepicker-date relative w-[2.7rem] h-[2.7rem] lg:w-10 lg:h-10 flex justify-center items-center text-xs 2xl:text-sm"
+            :class="[
+              datepickerClasses(date),
+              asRange ? 'transition-all' : 'transition-colors',
+            ]" :disabled="date.disabled || date.inRange()" :data-date="date.toDate()" @click="emit('updateDate', date)"
+            @mouseenter="atMouseOver(date)" @focusin="atMouseOver(date)" v-text="date.date()"
           />
-        </transition>
-        <button
-          type="button"
-          class="vtd-datepicker-date relative w-[2.7rem] h-[2.7rem] lg:w-10 lg:h-10 flex justify-center items-center text-xs 2xl:text-sm"
-          :class="[
-            datepickerClasses(date),
-            asRange ? 'transition-all' : 'transition-colors',
-          ]" :disabled="date.disabled || date.inRange()" :data-date="date.toDate()" @click="emit('updateDate', date)"
-          @mouseenter="atMouseOver(date)" @focusin="atMouseOver(date)" v-text="date.date()"
-        />
-      </div>
+        </div>
+      </template>
     </transition-group>
   </div>
 </template>
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import type { InjectionKey } from 'vue'
 import { inject } from 'vue'
+import type { Dayjs } from 'dayjs'
 
 export function injectStrict<T>(key: InjectionKey<T>, fallback?: T) {
   const resolved = inject(key, fallback)
@@ -9,3 +10,10 @@ export function injectStrict<T>(key: InjectionKey<T>, fallback?: T) {
 
   return resolved
 }
+
+
+export const localesMap = Object.fromEntries(
+  Object.entries(import.meta.glob('./locale/*.js')).map(
+    ([path, loadLocale]) => [path.match(/([\w-]*)\.js$/)?.[1], loadLocale],
+  ),
+) as Record<string, () => Promise<{ default: Dayjs['localeData'] }>>
